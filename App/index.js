a7a46bf1fa0d6735c6a2e97900be0055930a46d8
Ryan Yogan <ryanyogan@gmail.com>
2019-05-20 13:56:17 -0700

    silly math, just if statements to show how to use flex

--- App/index.js
+++ App/index.js
@@ -13,50 +13,144 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 40,
     textAlign: "right",
-    marginRight: 20
+    marginRight: 20,
+    marginBottom: 10
   }
 });
 
 export default class App extends React.Component {
+  state = {
+    currentValue: "0",
+    operator: null,
+    previousValue: null
+  };
+
+  handleTap = (type, value) => {
+    this.setState(state => {
+      if (type === "number") {
+        if (state.currentValue === "0") {
+          return { currentValue: `${value}` };
+        }
+
+        return {
+          currentValue: `${state.currentValue}${value}`
+        };
+      }
+
+      if (type === "operator") {
+        return {
+          operator: value,
+          previousValue: state.currentValue,
+          currentValue: "0"
+        };
+      }
+
+      if (type === "equal") {
+        const { currentValue, previousValue, operator } = state;
+        const current = parseFloat(currentValue);
+        const previous = parseFloat(previousValue);
+        const resetState = {
+          operator: null,
+          previousValue: null
+        };
+
+        if (operator === "/") {
+          return {
+            currentValue: previous / current,
+            ...resetState
+          };
+        }
+
+        if (operator === "*") {
+          return {
+            currentValue: previous * current,
+            ...resetState
+          };
+        }
+
+        if (operator === "+") {
+          return {
+            currentValue: previous + current,
+            ...resetState
+          };
+        }
+
+        if (operator === "-") {
+          return {
+            currentValue: previous - current,
+            ...resetState
+          };
+        }
+      }
+      return state;
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <SafeAreaView>
-          <Text style={styles.value}>75</Text>
+          <Text style={styles.value}>
+            {parseFloat(this.state.currentValue).toLocaleString()}
+          </Text>
 
           <Row>
             <Button text="C" theme="secondary" onPress={() => null} />
             <Button text="+/-" theme="secondary" onPress={() => null} />
             <Button text="%" theme="secondary" onPress={() => null} />
-            <Button text="/" theme="accent" onPress={() => null} />
+            <Button
+              text="/"
+              theme="accent"
+              onPress={() => this.handleTap("operator", "/")}
+            />
           </Row>
 
           <Row>
-            <Button text="7" onPress={() => null} />
-            <Button text="8" onPress={() => null} />
-            <Button text="9" onPress={() => null} />
-            <Button text="*" theme="accent" onPress={() => null} />
+            <Button text="7" onPress={() => this.handleTap("number", 7)} />
+            <Button text="8" onPress={() => this.handleTap("number", 8)} />
+            <Button text="9" onPress={() => this.handleTap("number", 9)} />
+            <Button
+              text="*"
+              theme="accent"
+              onPress={() => this.handleTap("operator", "*")}
+            />
           </Row>
 
           <Row>
-            <Button text="4" onPress={() => null} />
-            <Button text="5" onPress={() => null} />
-            <Button text="6" onPress={() => null} />
-            <Button text="-" theme="accent" onPress={() => null} />
+            <Button text="4" onPress={() => this.handleTap("number", 4)} />
+            <Button text="5" onPress={() => this.handleTap("number", 5)} />
+            <Button text="6" onPress={() => this.handleTap("number", 6)} />
+            <Button
+              text="-"
+              theme="accent"
+              onPress={() => this.handleTap("operator", "-")}
+            />
           </Row>
 
           <Row>
-            <Button text="1" onPress={() => null} />
-            <Button text="2" onPress={() => null} />
-            <Button text="3" onPress={() => null} />
-            <Button text="+" theme="accent" onPress={() => null} />
+            <Button text="1" onPress={() => this.handleTap("number", 1)} />
+            <Button text="2" onPress={() => this.handleTap("number", 2)} />
+            <Button text="3" onPress={() => this.handleTap("number", 3)} />
+            <Button
+              text="+"
+              theme="accent"
+              onPress={() => this.handleTap("operator", "+")}
+            />
           </Row>
 
           <Row>
-            <Button text="0" size="double" onPress={() => null} />
-            <Button text="." onPress={() => null} />
-            <Button text="=" theme="accent" onPress={() => null} />
+            <Button
+              text="0"
+              size="double"
+              onPress={() => this.handleTap("number", 0)}
+            />
+            <Button text="." onPress={() => this.handleTap("number", ".")} />
+            <Button
+              text="="
+              theme="accent"
+              onPress={() => this.handleTap("equal")}
+            />
           </Row>
         </SafeAreaView>
       </View>
